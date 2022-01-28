94d5cd378865fe5c4bbda12934a91a3f3f72d623
Josered30 <josered30@hotmail.com>
2022-01-27 20:19:32 -0500

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './modules/components/Header';
+
 import CourseSave from './modules/pages/courses/CourseSave';
 import CourseList from './modules/pages/courses/CourseList';
-
 import './App.css';
+
+const Courses = React.lazy(() => import('./modules/pages/courses/Courses'));
 
 function App(): React.ReactElement {
   return (
@@ -14,8 +16,18 @@ function App(): React.ReactElement {
         <Routes>
           <Route path="/" element={<p>Página de inicio en construcción</p>} />
           <Route path="*" element={<p>Página de inicio en construcción</p>} />
-          <Route path="/course/list" element={<CourseList />} />
-          <Route path="/course/save/:idCourse" element={<CourseSave />} />
+          <Route
+            path="course"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Courses />
+              </React.Suspense>
+            }
+          >
+            <Route index element={<CourseList />} />
+            <Route path="list" element={<CourseList />} />
+            <Route path="save/:idCourse" element={<CourseSave />} />
+          </Route>
         </Routes>
       </div>
     </div>
---
+++ src/modules/pages/courses/Courses.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+function Courses(): React.ReactElement {
+  return <Outlet />;
+}
+
+export default Courses;
